9ed7ccf8636637b0629fe756acbefa7428502043
zhang <757957839@qq.com>
2017-02-12 14:11:48 +0800

    新增登陆界面的logo，并更换了一种主题

--- app/src/main/java/com/tec/zhang/welcome.java
+++ app/src/main/java/com/tec/zhang/welcome.java
@@ -5,6 +5,7 @@ import android.os.Handler;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import org.litepal.tablemanager.Connector;
@@ -14,6 +15,7 @@ public class welcome extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         Connector.getDatabase();
         setContentView(R.layout.activity_welcome);
         if (!isServiceRunning()) {
